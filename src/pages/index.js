77ec598b7047477f75b7555a71388b9111a682cc
yoshixmk <pirori67@gmail.com>
2021-08-09 14:37:57 +0900

    add github icon and link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import { CategorySelect as CategorySelect } from 'components/CategorySelect';
 import { MenuSelect } from 'components/MenuSelect.js';
+import { Timer } from 'components/Timer';
 import { training } from 'domains/const/menus_const';
-import { Timer } from 'hooks/useTimer';
 import Head from 'next/head';
+import Image from 'next/image'
+import { DiGithubBadge } from "react-icons/di";
 import styles from 'styles/Home.module.css';
 
 export default function Home({ menus }) {
@@ -32,10 +34,15 @@ export default function Home({ menus }) {
             </div>
           ))}
         </div>
+
+        <Timer expiryTimestamp={time} />
       </main>
 
       <footer className={styles.footer}>
-        <Timer expiryTimestamp={time} />
+        <a href="https://github.com/yoshixmk/hiiter" target="_blank" rel="noreferrer">
+          <DiGithubBadge size="3em" />
+          <Image alt="GitHub stars" src="https://img.shields.io/github/stars/yoshixmk/hiiter?style=social" height="20" width="76" />
+        </a>
       </footer>
     </div>
   );
